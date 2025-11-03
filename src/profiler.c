45511d8f3c398b1cb8e352c605aa40b70839b20c
przbrz <przemyslaw.marek.brzezinski@gmail.com>
2025-11-03 22:00:24 +0100

    define basic structs and implement _profiler_clock_stack_add()

--- src/profiler.c
+++ src/profiler.c
@@ -1,2 +1,42 @@
 #include "profiler.h"
 
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct {
+    const char* label;
+    struct timespec begin;
+    struct timespec end;
+} Profiler_Clock;
+
+typedef struct {
+    Profiler_Clock* items;
+    size_t count;
+    size_t capacity;
+} Profiler_Clock_Stack;
+
+static Profiler_Clock_Stack _clock_stack = {0};
+
+#define _PROFILER_CLOCK_STACK_CAPACITY_INIT 8
+#define _PROFILER_CLOCK_STACK_CAPACITY_GROW 2
+static void _profiler_clock_stack_add(void)
+{
+    if (_clock_stack.capacity <= _clock_stack.count) {
+        if (_clock_stack.capacity == 0) {
+            _clock_stack.capacity = _PROFILER_CLOCK_STACK_CAPACITY_INIT;
+        }
+        else {
+            _clock_stack.capacity *= _PROFILER_CLOCK_STACK_CAPACITY_GROW;
+        }
+        Profiler_Clock* items_temp = (Profiler_Clock*)realloc(_clock_stack.items, sizeof(Profiler_Clock) * _clock_stack.capacity);
+        if (!items_temp) {
+            fprintf(stderr, "profiler: failed to grow clock stack\n");
+            exit(EXIT_FAILURE);
+        }
+        _clock_stack.items = items_temp;
+    }
+    Profiler_Clock clock = {0};
+    _clock_stack.items[_clock_stack.count++] = clock;
+}
+
--- src/profiler.h
+++ src/profiler.h
@@ -1,7 +1,8 @@
 #ifndef PROFILER_H
 #define PROFILER_H
 
-
+void profiler_clock_begin(const char* clock_label);
+void profiler_clock_end(void);
 
 #endif // PROFILER_H
 
