73bbbc56f7b42a94816ab39164d51388347f3082
przbrz <przemyslaw.marek.brzezinski@gmail.com>
2025-11-04 07:06:51 +0100

    add simple demo

--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
 #include "profiler.h"
 
-#include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    printf("profiler\n");
+    profiler_clock_begin("total");
+    profiler_clock_begin("first loop");
+    for (size_t i = 0; i < (1 << 20); ++i) {
+        rand();
+    }
+    profiler_clock_end();
+    profiler_clock_begin("second loop");
+    for (size_t i = 0; i < (1 << 16); ++i) {
+        rand();
+    }
+    profiler_clock_end();
+    profiler_clock_end();
     return 0;
 }
